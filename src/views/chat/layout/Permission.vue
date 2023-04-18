7020e653cfd6c13f1e37a3b49946303a23ff74a6
Miraz Hossain <miraz4300@gmail.com>
2023-04-18 20:10:43 +0000

    support for room specific prompt system

--- src/views/chat/layout/Permission.vue
+++ src/views/chat/layout/Permission.vue
@@ -192,7 +192,7 @@ async function handleResetPassword() {
 </script>
 
 <template>
-  <NModal :show="visible" style="width: 90%; max-width: 640px">
+  <NModal :show="visible" style="width: 90%; max-width: 440px">
     <div class="p-10 bg-white rounded dark:bg-slate-800">
       <div class="space-y-4">
         <header class="space-y-2">

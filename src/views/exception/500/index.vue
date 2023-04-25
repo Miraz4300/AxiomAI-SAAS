9dbd7190826ee92f71aa230c2a4c370c7a33b342
Miraz Hossain <miraz4300@gmail.com>
2023-04-25 06:08:08 +0000

    update error page

--- src/views/exception/500/index.vue
+++ src/views/exception/500/index.vue
@@ -12,17 +12,17 @@ function goHome() {
 <template>
   <div class="flex flex-col items-center justify-center h-screen text-center bg-[#D1D1D1] dark:bg-[#202020] text-black dark:text-white">
     <div class="flex items-center">
-      <h1 class="mr-5 pr-5 border-r-2 border-black dark:border-gray-500 h-14 text-2xl font-medium pt-2.5">
+      <h1 class="mr-6 pr-5 border-r-2 border-gray-500 dark:border-gray-500 h-14 text-3xl font-medium pt-2.5">
         500
       </h1>
       <div>
-        <h2 class="text-sm font-normal leading-1">
+        <h2 class="text-base font-medium leading-1">
           Internal server error.
         </h2>
       </div>
     </div>
     <br>
-    <p class="text-base text-center text-slate-500 dark:text-gray-500">
+    <p class="text-sm text-center text-gray-800 dark:text-gray-400">
       Don't worry, we're working to resolve the issue as quickly as possible.<br>
       you can check the realtime server status <a class="text-blue-600 dark:text-blue-500" href="https://axiomai.statuspage.io" target="_blank">here</a>
     </p>

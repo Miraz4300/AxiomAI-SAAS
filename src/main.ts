4c15b6006496767ca90e97fbbae15c2c1a2cd9b5
Miraz Hossain <miraz4300@gmail.com>
2023-05-24 07:39:55 +0000

    feat: Datadog RUM

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { datadogRum } from '@datadog/browser-rum'
 import App from './App.vue'
 import { setupI18n } from './locales'
 import { setupAssets, setupScrollbarStyle } from './plugins'
@@ -8,14 +9,28 @@ import { setupRouter } from './router'
 async function bootstrap() {
   const app = createApp(App)
   setupAssets()
-
   setupScrollbarStyle()
-
   setupStore(app)
-
   setupI18n(app)
-
   await setupRouter(app)
+
+  // Initialize Datadog RUM
+  datadogRum.init({
+    applicationId: 'a294f5a8-b059-4711-aadc-1c8e8a2c327d',
+    clientToken: 'pub978e5db4d73c2036c561d2dd9d8bca71',
+    site: 'datadoghq.com',
+    service: 'axiomai',
+    env: 'prod',
+    version: '2.6.0',
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+  })
+
+  datadogRum.startSessionReplayRecording()
 
   app.mount('#app')
 }

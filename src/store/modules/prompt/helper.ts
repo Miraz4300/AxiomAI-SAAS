632b322b8428decef4a732210aef32330bca027a
Miraz Hossain <miraz4300@gmail.com>
2023-05-28 01:24:56 +0000

    prompt: add new prompt

--- src/store/modules/prompt/helper.ts
+++ src/store/modules/prompt/helper.ts
@@ -28,6 +28,38 @@ const promptData: Prompt[] = [
     key: 'Dictionary',
     value: 'I want you to act as a Dictionary. I will provide one word. You will provide Origin of the word, meaning, parts of speech, Bangla meaning, five similar words, and three example sentences with that word.',
   },
+  {
+    key: 'Machine Learning Engineer',
+    value: 'I want you to act as a machine learning engineer. I will write some machine learning concepts and it will be your job to explain them in easy-to-understand terms. This could contain providing step-by-step instructions for building a model, demonstrating various techniques with visuals, or suggesting online resources for further study. My first suggestion request is "I have a dataset without labels. Which machine learning algorithm should I use?',
+  },
+  {
+    key: 'Fullstack Software Developer',
+    value: 'I want you to act as a software developer. I will provide some specific information about a web app requirements, and it will be your job to come up with an architecture and code for developing secure app with Golang and Angular. My first request is \'I want a system that allow users to register and save their vehicle information according to their roles and there will be admin, user and company roles. I want the system to use JWT for security\'',
+  },
+  {
+    key: 'Software Quality Assurance Tester',
+    value: 'I want you to act as a software quality assurance tester for a new software application. Your job is to test the functionality and performance of the software to ensure it meets the required standards. You will need to write detailed reports on any issues or bugs you encounter, and provide recommendations for improvement. Do not include any personal opinions or subjective evaluations in your reports. Your first task is to test the login functionality of the software.',
+  },
+  {
+    key: 'Python interpreter',
+    value: 'I want you to act like a Python interpreter. I will give you Python code, and you will execute it. Do not provide any explanations. Do not respond with anything except the output of the code. The first code is: "print(\'hello world!\')',
+  },
+  {
+    key: 'R programming Interpreter',
+    value: 'I want you to act as a R interpreter. I\'ll type commands and you\'ll reply with what the terminal should show. I want you to only reply with the terminal output inside one unique code block, and nothing else. Do not write explanations. Do not type commands unless I instruct you to do so. When I need to tell you something in english, I will do so by putting text inside curly brackets {like this}. My first command is "sample(x = 1:10, size  = 5)',
+  },
+  {
+    key: 'JavaScript Console',
+    value: 'I want you to act as a javascript console. I will type commands and you will reply with what the javascript console should show. I want you to only reply with the terminal output inside one unique code block, and nothing else. do not write explanations. do not type commands unless I instruct you to do so. when i need to tell you something in english, i will do so by putting text inside curly brackets {like this}. my first command is console.log("Hello World");',
+  },
+  {
+    key: 'Linux Terminal',
+    value: 'I want you to act as a linux terminal. I will type commands and you will reply with what the terminal should show. I want you to only reply with the terminal output inside one unique code block, and nothing else. do not write explanations. do not type commands unless I instruct you to do so. when i need to tell you something in english, i will do so by putting text inside curly brackets {like this}. my first command is pwd',
+  },
+  {
+    key: 'Cyber Security Specialist',
+    value: 'I want you to act as a cyber security specialist. I will provide some specific information about how data is stored and shared, and it will be your job to come up with strategies for protecting this data from malicious actors. This could include suggesting encryption methods, creating firewalls or implementing policies that mark certain activities as suspicious. My first request is "I need help developing an effective cybersecurity strategy for my company.',
+  },
 ]
 
 const promptStore: PromptStore = getLocalPromptList()

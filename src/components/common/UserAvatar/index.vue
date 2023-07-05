141b0e356a3feaabfb7166f868677f04ee0dd7a4
Miraz Hossain <miraz4300@gmail.com>
2023-07-05 10:23:30 +0000

    fix: change default avatar

--- src/components/common/UserAvatar/index.vue
+++ src/components/common/UserAvatar/index.vue
@@ -2,7 +2,6 @@
 import { computed } from 'vue'
 import { NAvatar } from 'naive-ui'
 import { useUserStore } from '@/store'
-import defaultAvatar from '@/assets/avatar.jpg'
 import { isString } from '@/utils/is'
 
 const props = defineProps({
@@ -15,6 +14,8 @@ const props = defineProps({
 const userStore = useUserStore()
 
 const userInfo = computed(() => userStore.userInfo)
+
+const defaultAvatar = '/assets/avatar_1.jpg'
 </script>
 
 <template>

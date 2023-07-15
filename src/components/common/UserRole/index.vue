8fb2d824b757a5e3e8cfc7979744bb86c887b2ee
Miraz Hossain <miraz4300@gmail.com>
2023-07-15 18:17:28 +0000

    tag type based on the user role

--- src/components/common/UserRole/index.vue
+++ src/components/common/UserRole/index.vue
@@ -6,10 +6,23 @@ import { useUserStore } from '@/store'
 
 const userStore = useUserStore()
 const userInfo = computed(() => userStore.userInfo)
+
+function Tag(role: UserRole) {
+  if (role === UserRole.Free)
+    return 'default'
+  else if (role === UserRole.Premium)
+    return 'success'
+  else if (role === UserRole.MVP)
+    return 'warning'
+  else if (role === UserRole.Support)
+    return 'info'
+  else
+    return 'default'
+}
 </script>
 
 <template>
-  <NTag v-if="userInfo.roles.length > 0" size="small" :bordered="false" type="success">
+  <NTag v-if="userInfo.roles.length > 0" size="small" :bordered="false" :type="Tag(userInfo.roles[0])">
     {{ UserRole[userInfo.roles[0]] }}
   </NTag>
 </template>

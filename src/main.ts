d06919116c31afd36d7d6a48e86b0e7cb000c722
Miraz Hossain <miraz4300@gmail.com>
2023-07-15 11:29:47 +0000

    update to version 3.0.0-preview6

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     site: 'datadoghq.com',
     service: 'axiomai',
     env: 'prod',
-    version: '3.0.0-preview5',
+    version: '3.0.0-preview6',
     sessionSampleRate: 100,
     sessionReplaySampleRate: 100,
     trackUserInteractions: true,

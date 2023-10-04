e55c0abcbc582418d3f00dbc76d0d0487f6a59ee
Miraz Hossain <31208996+Miraz4300@users.noreply.github.com>
2023-10-04 11:47:49 +0000

    chore: improved code structure of Merch

--- src/components/common/Setting/Merch.vue
+++ src/components/common/Setting/Merch.vue
@@ -9,16 +9,11 @@ import { fetchUserMerch } from '@/api'
 const appStore = useAppStore()
 const theme = computed(() => appStore.theme)
 const { isMobile } = useBasicLayout()
-
-const lightBanner = ref('')
-const darkBanner = ref('')
 const merchConfig = ref<MerchConfig>()
 
 onMounted(async () => {
   const response = await fetchUserMerch<MerchConfig>()
   merchConfig.value = response.data
-  lightBanner.value = merchConfig.value?.lightBanner || ''
-  darkBanner.value = merchConfig.value?.darkBanner || ''
 })
 </script>
 
@@ -26,8 +21,8 @@ onMounted(async () => {
   <div class="space-y-4">
     <div class="flex justify-center">
       <div class="w-auto">
-        <img v-if="theme === 'light'" :src="lightBanner">
-        <img v-else :src="darkBanner">
+        <img v-if="theme === 'light'" :src="merchConfig?.lightBanner">
+        <img v-else :src="merchConfig?.darkBanner">
       </div>
     </div>
     <div class="pl-4 pr-4 flex justify-center" :class="[isMobile ? 'flex-col space-y-4' : 'space-x-4']">

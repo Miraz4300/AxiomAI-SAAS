0c49a25c4a4cbb92129a9e82677dd02c3cd1cc2c
Miraz Hossain <31208996+Miraz4300@users.noreply.github.com>
2023-11-30 19:08:35 +0000

    fix: modal width at small screen

--- src/components/admin/Keys.vue
+++ src/components/admin/Keys.vue
@@ -234,7 +234,7 @@ onMounted(async () => {
     </div>
   </div>
 
-  <NModal v-model:show="show" :auto-focus="false" preset="card" style="width: 50%">
+  <NModal v-model:show="show" :auto-focus="false" preset="card" style="width: 95%; max-width: 980px">
     <div class="p-4 space-y-5 min-h-[200px]">
       <div class="space-y-6">
         <div class="flex items-center space-x-4">

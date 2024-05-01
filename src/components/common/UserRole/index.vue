a54dc6a97fba20141a33d2a487de62524dc857f3
Md Miraz Hossain <miraz4300@gmail.com>
2024-05-01 20:29:10 +0600

    fix: role card logics and clean code

--- src/components/common/UserRole/index.vue
+++ src/components/common/UserRole/index.vue
@@ -31,6 +31,10 @@ function goSub() {
   router.push('/settings?id=subscription')
 }
 
+function goGen() {
+  router.push('/settings?id=general')
+}
+
 function TagType(role: UserRole) {
   if (role === UserRole.Premium)
     return 'success'
@@ -73,8 +77,8 @@ const Icon = computed(() => IconName(Role))
   <div v-if="props.subLink" class="flex flex-col bg-[#F0F4F9] dark:bg-[#161B22] py-2 px-3 rounded-lg mt-2">
     <a v-if="Role !== UserRole.Admin" class="font-bold mb-1">Subscription details:</a>
     <a> Role:
-      <NTag v-if="userInfo.roles.length > 0" size="small" :bordered="false" :type="Tag" @click="goSub">
-        <a class="cursor-pointer font-semibold">{{ UserRole[userInfo.roles[0]] }}</a>
+      <NTag v-if="userInfo.roles.length > 0" size="small" :bordered="false" :type="Tag">
+        <a class="font-semibold">{{ UserRole[userInfo.roles[0]] }}</a>
         <template v-if="Icon && props.icon" #icon>
           <SvgIcon :icon="Icon" />
         </template>
@@ -82,9 +86,9 @@ const Icon = computed(() => IconName(Role))
     </a>
     <a> {{ userInfo.remark }} </a>
     <a v-if="Role === UserRole.Free" class="cursor-pointer text-[#fb923c]" @click="goSub">Buy subscription here</a>
-    <a v-if="Role === UserRole.Admin" class="cursor-pointer text-[#14b8a6] font-semibold" @click="goSub">Administrator Account</a>
+    <a v-if="Role === UserRole.Admin" class="text-[#14b8a6] font-semibold">Administrator Account</a>
   </div>
-  <NTag v-if="props.onlyTag && userInfo.roles.length > 0" size="small" :bordered="false" :type="Tag" @click="goSub">
+  <NTag v-if="props.onlyTag && userInfo.roles.length > 0" size="small" :bordered="false" :type="Tag" @click="goGen">
     <a class="cursor-pointer font-semibold">{{ UserRole[userInfo.roles[0]] }}</a>
     <template v-if="Icon && props.icon" #icon>
       <SvgIcon :icon="Icon" />

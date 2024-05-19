4893d01f50877b64e0ef5ce2aa205cb62c9b189f
Md Miraz Hossain <miraz4300@gmail.com>
2024-05-19 13:45:33 +0600

    chore: update tos and login ui

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,7 +4,7 @@ import AuthIntro from './components/AuthIntro.vue'
 </script>
 
 <template>
-  <div class="flex min-h-full w-screen flex-col sm:supports-[min-height:100dvh]:min-h-[100dvh] md:grid md:grid-cols-2 lg:grid-cols-[60%_40%]">
+  <div class="flex min-h-full w-screen flex-col sm:supports-[min-height:100dvh]:min-h-[100dvh] md:grid md:grid-cols-2 lg:grid-cols-[55%_45%]">
     <AuthIntro />
     <AuthLayout />
   </div>
